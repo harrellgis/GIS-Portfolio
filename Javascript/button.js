8b9bb511a268a07e7774a86c35e95a0c5bb7e722
Chris Harrell <harrellmzungu@gmail.com>
2020-04-19 19:20:28 +0300

    Changed styling of primary buttons Created a javascript file for button animation

---
+++ Javascript/button.js
@@ -0,0 +1,17 @@
+//Button animation
+const buttons = document.querySelectorAll(".primary-button a");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement("span");
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+      ripples.remove();
+    }, 1000);
+  });
+});
