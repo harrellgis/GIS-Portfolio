3743c242ff68493bc1c79d78a910ece46fbd7a8d
Chris Harrell <harrellmzungu@gmail.com>
2020-11-11 21:38:31 +0300

    Fixed mobile nav link fade animation Changed burger markup Added burger animation

--- Javascript/nav.js
+++ Javascript/nav.js
@@ -1,24 +1,22 @@
 //Navbar functionality for mobile//
 const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links div");
+  const burger = document.querySelector('.menu-btn');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links a');
 
-  burger.addEventListener("click", () => {
+  burger.addEventListener('click', () => {
     //Toggle nav//
-    nav.classList.toggle("nav-active");
+    nav.classList.toggle('nav-active');
 
     //Animate links//
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        link.style.animation = "";
+        link.style.animation = '';
       } else {
         link.style.animation =
-          "navLinkFade 0.5s ease forwards ${index / 7 + 2}s";
+          `navLinkFade 1s ease forwards ${index / 7 + 0.5}s`;
       }
     });
-    //Burger Animation
-    burger.classList.toggle("toggle-burger");
   });
 };
 
